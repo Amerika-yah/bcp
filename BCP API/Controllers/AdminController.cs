d34b383cf8b9b850eecb94f91a95878087db2f83
hilawnabangus <jaymaryugo90@gmail.com>
2024-05-10 14:16:29 +0800

    Update AdminController

--- BCP API/Controllers/AdminController.cs
+++ BCP API/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
 ﻿using BCP_API.Data;
+using BCP_API.Entity;
+using BCP_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,10 +23,27 @@ namespace BCP_API.Controllers
 
         public IActionResult Get(int pageIndex = 0, int pageSize = 10)
         {
-            var userCount = _dbContext.Users.Count();
-            var userList = _dbContext.Users.Include(x => x.EmpID).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+            BaseResponseModel response = new BaseResponseModel();
 
-            return View();
+            try
+            {
+                var userCount = _dbContext.Users.Count();
+                var userList = _dbContext.Users.Include(x => x.EmpID).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+
+                response.Status = true;
+                response.Message = "Success";
+                response.Data = new { Users = userList, Count = userCount };
+
+                //return View();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+                Console.WriteLine(response.Message);
+                throw;
+            }
         }
 
     }
