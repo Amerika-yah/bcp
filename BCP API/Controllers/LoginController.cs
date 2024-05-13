1064def4ddb0e04df0527c8675263959520c12d6
Yabetsu <it.jabez16@gmail.com>
2024-05-13 15:10:34 +0800

    Add LoginController

---
+++ BCP API/Controllers/LoginController.cs
@@ -0,0 +1,116 @@
+﻿using Azure;
+using BCP_API.Data;
+using BCP_API.Entity;
+using BCP_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BCP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private readonly BCPDbContext _dbContext;
+
+        public LoginController(BCPDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            BaseResponseModel response = new BaseResponseModel();
+
+            try
+            {
+                var userCount = _dbContext.Users.Count();
+                var userList = _dbContext.Users.ToList();
+
+                response.Status = true;
+                response.Message = "Success";
+                response.Data = new { Users = userList, Count = userCount };
+
+                //return View();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+                Console.WriteLine(response.Message);
+                //throw;
+                return BadRequest(response);
+            }
+        }
+
+        [HttpPost("{email}/{password}")]
+        public IActionResult Login(string email, string password)
+        {
+            BaseResponseModel response = new BaseResponseModel();
+
+            try
+            {
+                var encryptedPassword = GetMD5(password);
+                var data = _dbContext.Users.Where(u => u.Email.Equals(email) && u.Password.Equals(encryptedPassword)).ToList();
+
+                if (data.IsNullOrEmpty())
+                {
+                    response.Status = false;
+                    response.Message = "User Not Found.";
+
+                    return BadRequest(response);
+                }
+                else
+                {
+                    response.Status = true;
+                    response.Message = "Success";
+                    response.Data = data;
+
+                    return Ok(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+                Console.WriteLine(response.Message);
+
+                return BadRequest(response);
+            }
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+
+        // TODO: Forgot Password
+        [HttpPost("forgot-password")]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [NonAction]
+        public static string GetMD5(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(text);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byteToString = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byteToString += targetData[i].ToString("x2");
+            }
+
+            return byteToString;
+        }
+    }
+}
--- BCP API/obj/Debug/net8.0/BCP API.AssemblyInfo.cs
+++ BCP API/obj/Debug/net8.0/BCP API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BCP API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1225f1260fcc08edd2b32c1feea708c40a332fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1452da437057e757518010eb9839e49651417216")]
 [assembly: System.Reflection.AssemblyProductAttribute("BCP API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BCP API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
