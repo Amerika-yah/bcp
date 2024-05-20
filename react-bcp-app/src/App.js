31466d3bfbd30791eccf7cb53d3752653a7e5149
Yabetsu <it.jabez16@gmail.com>
2024-05-20 13:53:04 +0800

    Add packages, modify App.js

--- react-bcp-app/src/App.js
+++ react-bcp-app/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <BrowserRouter>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand as={Link} to="/">BCP</Navbar.Brand>
+          <Nav className='mr-auto'>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          {/* <Route exact path="/" Component={() => } */}
+        </Switch>
+      </BrowserRouter>
+    </Container>
   );
 }
 
