b34b37ef4f29b8d3371caa3eb6d5fb63e6c1cda3
hilawnabangus <jaymaryugo90@gmail.com>
2024-05-21 14:06:30 +0800

    Update App.js
    added dashboard link

--- react-bcp-app/src/App.js
+++ react-bcp-app/src/App.js
@@ -16,6 +16,7 @@ function App() {
         </Navbar>
         <Switch>
           {/* <Route exact path="/" Component={() => } */}
+          <Route exact path="/admin/dashboards" component={Actors} />
         </Switch>
       </BrowserRouter>
     </Container>
